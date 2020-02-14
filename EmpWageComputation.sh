57c69481ce41c7c71f83b7ce3fcebc7f35a57271
Kartikey45 <singhkartikey45@gmail.com>
2020-02-14 18:25:12 +0530

    ADDED:Calculate Daily Employee Wage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -4,12 +4,20 @@ echo "Welcome to Employee Wage Computation"
 
 #INITIALIZE THE VARIABLE
 IS_PRESENT=1
+WAGE_PER_HOUR=20
 
 #CHECK THE CONDITIONS AND DISPLAY THE ATTENDANCE
 echo "Attandance of Employee :"
 if (( $(( RANDOM%2 ))==$IS_PRESENT ))
 then
 	echo "Employee is present"
+	hoursPerDay=8
 else
 	echo "Employee is Absent"
+	hoursPerDay=0
 fi
+
+#CALCULATE DAILY EMPLOYEE WAGE
+echo "Daily Employee Wage :"
+dailyEmployeeWage=$(($WAGE_PER_HOUR*$hoursPerDay))
+echo "$dailyEmployeeWage rs. per day"
