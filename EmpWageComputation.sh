b3703a3e042b91a6c2affe24f5b1608ee40dc2e7
Kartikey45 <singhkartikey45@gmail.com>
2020-02-14 17:49:01 +0530

    ADDED:Attendance of an Employee

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,3 +1,15 @@
 #!/bin/bash -x 
 
 echo "Welcome to Employee Wage Computation"
+
+#INITIALIZE THE VARIABLE
+IS_PRESENT=1
+
+#CHECK THE CONDITIONS AND DISPLAY THE ATTENDANCE
+echo "Attandance of Employee :"
+if (( $(( RANDOM%2 ))==$IS_PRESENT ))
+then
+	echo "Employee is present"
+else
+	echo "Employee is Absent"
+fi
