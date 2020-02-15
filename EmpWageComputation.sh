03b915854a05c8987f57a5979932f72fb77233a4
Kartikey45 <singhkartikey45@gmail.com>
2020-02-15 09:03:31 +0530

    ADDED:Part time Employee & Wage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -2,22 +2,28 @@
 
 echo "Welcome to Employee Wage Computation"
 
-#INITIALIZE THE VARIABLE
-IS_PRESENT=1
+#INITIALIZE THE CONSTANT VARIABLES
 WAGE_PER_HOUR=20
+IS_PART_TIME=1
+IS_FULL_TIME=2
 
-#CHECK THE CONDITIONS AND DISPLAY THE ATTENDANCE
-echo "Attandance of Employee :"
-if (( $(( RANDOM%2 ))==$IS_PRESENT ))
+#CHECK THE CONDITIONS - IF AN EMPLOYEE IS A PART TIME OR FULL TIME EMPLOYEE
+if (( $(( RANDOM%3 ))==$IS_FULL_TIME ))
 then
-	echo "Employee is present"
+	echo "This is a full time employee"
 	hoursPerDay=8
+elif(( $(( RANDOM%3 ))==$IS_PART_TIME ))
+then
+	echo "This is a part time employee"
+	hoursPerDay=4
 else
-	echo "Employee is Absent"
+	echo "Employee is absent"
 	hoursPerDay=0
 fi
 
-#CALCULATE DAILY EMPLOYEE WAGE
+#CALCULATE AND DISPLAY DAILY EMPLOYEE WAGE
 echo "Daily Employee Wage :"
 dailyEmployeeWage=$(($WAGE_PER_HOUR*$hoursPerDay))
 echo "$dailyEmployeeWage rs. per day"
+
+
