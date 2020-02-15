da2091a63d2b4acb152246997492f206d5de7b23
Kartikey45 <singhkartikey45@gmail.com>
2020-02-15 10:38:33 +0530

    ADDED:Display Part time Employee & Wage using Case statement

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -2,24 +2,27 @@
 
 echo "Welcome to Employee Wage Computation"
 
-#INITIALIZE THE CONSTANT VARIABLES
+#INITIALIZE THE VARIABLES
 WAGE_PER_HOUR=20
 IS_PART_TIME=1
 IS_FULL_TIME=2
+checkEmp=$(( RANDOM%3 ))
 
 #CHECK THE CONDITIONS - IF AN EMPLOYEE IS A PART TIME OR FULL TIME EMPLOYEE
-if (( $(( RANDOM%3 ))==$IS_FULL_TIME ))
-then
-	echo "This is a full time employee"
-	hoursPerDay=8
-elif(( $(( RANDOM%3 ))==$IS_PART_TIME ))
-then
-	echo "This is a part time employee"
-	hoursPerDay=4
-else
-	echo "Employee is absent"
-	hoursPerDay=0
-fi
+case $checkEmp in	
+	$IS_FULL_TIME)
+		echo "This is a Full time Employee."
+		hoursPerDay=8
+		;;
+	$IS_PART_TIME)
+		echo "This is a Part time Employee."
+		hoursPerDay=4
+		;;
+	*)
+		echo "This is neither a Full time nor a Part time Employee."
+		hoursPerDay=0
+		;;
+esac
 
 #CALCULATE AND DISPLAY DAILY EMPLOYEE WAGE
 echo "Daily Employee Wage :"
