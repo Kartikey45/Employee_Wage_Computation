d842c21b865d0d6451b2c883742be3eb0d0b3a0f
Kartikey45 <singhkartikey45@gmail.com>
2020-02-15 13:08:00 +0530

    ADDED:Calculating Wages for a month

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -2,31 +2,39 @@
 
 echo "Welcome to Employee Wage Computation"
 
-#INITIALIZE THE VARIABLES
+#INITIALIZE THE VARIABLES AND CONSTANTS
 WAGE_PER_HOUR=20
 IS_PART_TIME=1
 IS_FULL_TIME=2
-checkEmp=$(( RANDOM%3 ))
+WORKING_DAYS_PER_MONTH=20
+salary=0
 
-#CHECK THE CONDITIONS - IF AN EMPLOYEE IS A PART TIME OR FULL TIME EMPLOYEE
-case $checkEmp in	
-	$IS_FULL_TIME)
-		echo "This is a Full time Employee."
-		hoursPerDay=8
-		;;
-	$IS_PART_TIME)
-		echo "This is a Part time Employee."
-		hoursPerDay=4
-		;;
-	*)
-		echo "This is neither a Full time nor a Part time Employee."
-		hoursPerDay=0
-		;;
-esac
+#CHECK THE CONDITION - EMPLOYEE IS A PART TIME OR FULL TIME EMPLOYEE 
+for((days=1; days<=20; days++))
+do
 
-#CALCULATE AND DISPLAY DAILY EMPLOYEE WAGE
-echo "Daily Employee Wage :"
-dailyEmployeeWage=$(($WAGE_PER_HOUR*$hoursPerDay))
-echo "$dailyEmployeeWage rs. per day"
+	#CHECK EMPLOYEE WORKS FOR FULL TIME OR PART TIME IN A DAY
+	checkEmp=$(( RANDOM%3 ))
+	
+	case $checkEmp in	
+		$IS_FULL_TIME)
+			hoursPerDay=8
+			;;
+		$IS_PART_TIME)
+			hoursPerDay=4
+			;;
+		*)
+			hoursPerDay=0
+			;;
+	esac
+
+	#CALCULATE EMPLOYEE WAGES FOR A MONTH
+	dailyEmployeeWage=$(($WAGE_PER_HOUR*$hoursPerDay)) 
+	salary=$(( $salary+$dailyEmployeeWage ))
+done
+
+#DISPLAY EMPLOYEE'S WAGES FOR A MONTH
+echo "Employee's wages for a month :"
+echo "$salary rs. per month"
 
 
